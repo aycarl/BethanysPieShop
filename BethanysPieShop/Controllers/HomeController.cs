287ca610d69dbd539cf10443f890787623c2c918
Carl <carl.agbenyega@gmail.com>
2019-06-03 18:02:18 -0400

    First commit
    creating MVC application from scratch: creating base a model, view and controller

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var HomeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to the Pie Shop!",
+                Pies = pies.ToList()
+            };
+
+            return View(HomeViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set;}
+
+    }
+}
