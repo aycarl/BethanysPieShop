b0a3677859b7eed3536c14fe6027575d8c675c56
Carl <carl.agbenyega@gmail.com>
2019-06-05 16:01:03 -0400

    Adding EF Core, Client-side package additions, database connnections & migrations
    Adding EF Core, Created database using migrations, creating connection strings

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -7,16 +7,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BethanysPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IPieRepository, PieRepository>();
 
             services.AddMvc();
         }
